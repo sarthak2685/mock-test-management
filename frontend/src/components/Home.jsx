18fb56d9c46827f5e0d2477576c54e76a492a040
KARAN KUMAR GUPTA <107607829+karang152@users.noreply.github.com>
2024-10-04 00:38:04 +0530

    why-us section created (#5)
    dark mode remove from subscription
    and why us section created

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './Home/banner.jsx'
 import Subscription from './Home/subscription.jsx'
+import WhyUs from './Home/why-us.jsx'
 
 function Home() {
   return (
     <>
     <Banner />
     <Subscription />
+    <WhyUs />
     </>
   )
 }
