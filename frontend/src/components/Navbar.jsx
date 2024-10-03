c19a7094457229c720d5539295336e43237dbbed
sarthak <sarthaks2711btechcse2025@kccitm.edu.in>
2024-10-04 00:37:38 +0530

    login done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,9 +12,13 @@ const Navbar = () => {
     <nav className="bg-[#FCFCFC] text-black shadow-md border-b border-gray-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
+         
           <div className="flex items-center">
-            <span className="text-2xl font-bold hover:text-[#007bff]">Mock Period</span>
+          <Link to="/">
+            <span className="text-2xl  hover:text-[#007bff]">Mock <strong className='text-[#007bff]'>Period</strong></span>
+            </Link>
           </div>
+        
           <div className="hidden md:flex items-center space-x-9">
             <Link
               to="/contact"
