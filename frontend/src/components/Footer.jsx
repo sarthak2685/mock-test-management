ca213acd6f53de94f1003db191f3691e8e758b9d
Nitin Kumar <nitink710btechaiml2025@kccitm.edu.in>
2024-10-03 23:14:48 +0530

    Footer Updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,86 +1,105 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-/*import logo from '../../assets/images/logo.png';*/
-import {  FaYoutube, FaXTwitter } from "react-icons/fa6";
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaYoutube, FaXTwitter } from "react-icons/fa6";
 import { AiFillInstagram } from "react-icons/ai";
+import { IoCall, IoLocationSharp } from "react-icons/io5";
+import { MdEmail } from "react-icons/md";
 
 const socialLinks = [
   {
-    path: 'https://www.instagram.com/',
+    path: "https://www.instagram.com/",
     icon: <AiFillInstagram />,
   },
   {
-    path: 'https://x.com/',
+    path: "https://x.com/",
     icon: <FaXTwitter />,
   },
   {
-    path: 'https://www.youtube.com/c/',
-    icon: <FaYoutube/>,
+    path: "https://www.youtube.com/c/",
+    icon: <FaYoutube />,
   },
 ];
 
 const reach = [
-  { display: 'Buxar,India' },
-  { display: 'Call 9911334455' },
-  { display: 'xcv@gmail.com' },
+  {
+    display: (
+      <>
+        <IoLocationSharp className="inline-block mr-2" /> Buxar, India
+      </>
+    ),
+  },
+  {
+    display: (
+      <>
+        <IoCall className="inline-block mr-2" /> +91-9911334455
+      </>
+    ),
+  },
+  {
+    display: (
+      <>
+        <MdEmail className="inline-block mr-2" /> xcv@gmail.com
+      </>
+    ),
+  },
 ];
 
 const company = [
-   { path: '/about-us', display: 'About Us' },
-   { path: '/privacy-policy', display: 'Privacy Policy' },
-   { path: '/terms-condition', display: 'Terms And Condition' },
+  { path: "/why-us", display: "Why Us" },
+  { path: "/privacy-policy", display: "Privacy Policy" },
+  { path: "/terms-condition", display: "Terms And Condition" },
 ];
 
 // Footer component
 const Footer = () => {
-
   return (
-    <footer className="pb-16 pt-10 bg-blue-600 text-white">
-      <br></br>
-      <br></br>
-      <br></br>
-      <div className="container">
+    <footer className="pb-16 pt-10 bg-[#FCFCFC] text-black shadow-lg border-t-2 border-gray-200">
+      <br />
+      <br />
+      <br />
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between flex-col md:flex-row flex-wrap gap-[30px] text-left">
-
           <div className="-mt-6">
             {/* Logo Section */}
-
-            <h3 className="text-[35px] font-[400] text-white mt-4">Mock <b>Period.</b></h3>
-            <p className="text-[16px] font-[400] text-white mt-4">
+            <h3 className="text-[35px] font-[400] text-black mt-4">
+              Mock <b>Period.</b>
+            </h3>
+            <p className="text-[16px] font-[400] text-black mt-4">
               Lorem ipsum is placeholder text commonly
-              <br></br>
+              <br />
               used in the graphic, print, and publishing
-              <br></br>
+              <br />
               industries for previewing layouts and visual
-              <br></br>
+              <br />
               mockups.
             </p>
           </div>
 
+          <div className="flex items-center gap-4 mt-4">
+            {socialLinks.map((link, index) => (
+              <Link
+                to={link.path}
+                key={index}
+                className="w-12 h-12 flex items-center justify-center group social-link"
+                aria-label={link.ariaLabel}
+              >
+                <div className="text-[27px]">{link.icon}</div>
+              </Link>
+            ))}
+          </div>
 
-          <div className="flex items-center gap-3 mt-4">
-              {socialLinks.map((link, index) => (
-                <Link
-                  to={link.path}
-                  key={index}
-                  className="w-9 h-9  flex items-center justify-center group  social-link"
-                  aria-label={link.ariaLabel}
-                >
-                  {link.icon}
-                </Link>
-              ))}
-            </div>
-
-
-          {/*Reach Out To Us*/}
+          {/* Reach Out To Us */}
           <div>
-            <h2 className="text-[20px] leading-[30px] font-[700] mb-3 text-white">
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-3 text-black">
               Reach Out To Us
             </h2>
             <ul>
               {reach.map((item, index) => (
                 <li key={index} className="mb-4 text-right">
-                  <Link to={item.path} className="text-[16px] leading-7 font-[400] text-white ">
+                  <Link
+                    to={item.path}
+                    className="text-[16px] leading-7 font-[400] text-black"
+                  >
                     {item.display}
                   </Link>
                 </li>
@@ -88,24 +107,24 @@ const Footer = () => {
             </ul>
           </div>
 
-
-
           {/* Company */}
           <div>
-            <h2 className="text-[20px] leading-[30px] font-[700] mb-3 text-white text-right">
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-3 text-black text-right">
               Company
             </h2>
             <ul>
               {company.map((item, index) => (
                 <li key={index} className="mb-4 text-right">
-                  <Link to={item.path} className="text-[16px] leading-7 font-[400] text-white ">
+                  <Link
+                    to={item.path}
+                    className="text-[16px] leading-7 font-[400] text-black"
+                  >
                     {item.display}
                   </Link>
                 </li>
               ))}
             </ul>
           </div>
-
         </div>
       </div>
     </footer>
