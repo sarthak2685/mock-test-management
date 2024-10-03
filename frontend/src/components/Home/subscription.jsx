4247a7b5ef18d566b3bb26637be3e6834c774ee4
KARAN KUMAR GUPTA <karang152btechaiml2025@kccitm.edu.in>
2024-10-04 00:15:44 +0530

    why-us section created
    dark mode remove from subscription
    and why us section created

--- frontend/src/components/Home/subscription.jsx
+++ frontend/src/components/Home/subscription.jsx
@@ -7,13 +7,13 @@ function Subscription() {
     <section className="bg-white dark:bg-gray-900">
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div className="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+          <h1 className="mb-4 text-6xl tracking-tight font-extrabold text-gray-900 dark:text-white">
             Subscription
             <span className="text-[#007bff]"> Plan</span>
-          </h2>
-          <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">
+          </h1>
+          <h6 className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">
             Get access to premium mock tests.
-          </p>
+          </h6>
         </div>
         {/* Responsive Grid */}
         <div className="space-y-8 md:grid md:grid-cols-2 md:gap-6 xl:gap-10 md:space-y-0">
