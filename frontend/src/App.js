45c18a77c9ecadf66062bfba17053feb8fcc8f9d
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2024-10-30 16:46:55 +0530

    Super Admin (#32)
    Co-authored-by: Sarthak Rai <106958899+sarthak2685@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar.jsx';
-import Footer from './components/Footer.jsx';
-import Home from './components/Home.jsx';
-import Login from './components/Login.jsx';
-import ContactPage from './components/Contact.jsx';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+import Home from "./components/Home.jsx";
+import Login from "./components/Login.jsx";
+import ContactPage from "./components/Contact.jsx";
+import AdminManagement from "./components/SuperAdminDashboard/AdminManagement.jsx";
+import StudentManagement from "./components/AdminDasboard/StudentMangement.jsx";
+import MockTestManagement from "./components/SuperAdminDashboard/MockTest.jsx";
+import ChartComponent from "./components/AdminDasboard/Perfomance.jsx";
+import Dashboard from "./components/AdminDasboard/Dasboard.jsx";
+import MockDemo from "./components/Home/mock-demo.jsx";
+import StudentPerformance from "./components/AdminDasboard/StudentPerformance.jsx";
+import SuperAdminDashboard from "./components/SuperAdminDashboard/SuperAdminDashboard.jsx";
 import AdminDashboard from './components/AdminDasboard/AdminDasboard.jsx';
-import StudentManagement from './components/AdminDasboard/StudentMangement.jsx';
-import MockTestManagement from './components/AdminDasboard/MockTest.jsx';
-import ChartComponent from './components/AdminDasboard/Perfomance.jsx';
-import Dashboard from './components/AdminDasboard/Dasboard.jsx';
-import MockDemo from './components/Home/Mock/mock-demo.jsx';
-import StudentPerformance from './components/AdminDasboard/StudentPerformance.jsx';
 import Dashboards from './components/StudentDashboard/Dashboards.jsx';
 import Performances from './components/StudentDashboard/Performances.jsx';
 import StudentPerformances from './components/StudentDashboard/StudentPerformances.jsx';
@@ -22,15 +24,23 @@ function App() {
     <Router>
       <div className="App">
         {/* Conditional Navbar and Footer */}
-        {['/', '/login', '/contact'].includes(window.location.pathname) && <Navbar />}
+        {["/", "/login", "/contact"].includes(window.location.pathname) && (
+          <Navbar />
+        )}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/admin" element={<Dashboard />} />
+          <Route path="/super-admin" element={<SuperAdminDashboard />} />
           <Route path="/students" element={<StudentManagement />} />
           <Route path="/create-test" element={<MockTestManagement />} />
           <Route path="/performance" element={<ChartComponent />} />
+          <Route path="/admin-management" element={<AdminManagement />} />
+          <Route
+            path="/student-performance/:id"
+            element={<StudentPerformance />}
+          />
           < Route path='/mock-demo' element={<MockDemo />} />
           <Route path="/student-performance/:id" element={<StudentPerformance />} />
           <Route path="/student-dashboard" element={<Dashboards />} />
@@ -39,7 +49,9 @@ function App() {
           <Route path="/profile" element={<Profile />} />
           <Route path="/help" element={<Help />} />
         </Routes>
-        {['/', '/login', '/contact'].includes(window.location.pathname) && <Footer />}
+        {["/", "/login", "/contact"].includes(window.location.pathname) && (
+          <Footer />
+        )}
       </div>
     </Router>
   );
