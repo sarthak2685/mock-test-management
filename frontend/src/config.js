7937366787f72cf1f79dec660cfd9f31d06ca7bf
Sarthak Rai <106958899+sarthak2685@users.noreply.github.com>
2025-07-09 12:54:18 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const config = {
     // apiUrl: "https://api.mockperiod.com",
-    apiUrl: "https://nyctisd.in.crpch.in",
+    apiUrl: "https://cod.crpch.in",
     // apiUrl: 'https://mockexam.pythonanywhere.com',
 };
 
